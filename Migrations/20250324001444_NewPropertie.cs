36e03508faf46b185363ce78786b04a5042b8d89
Raykusen <30479285+Raykusen@users.noreply.github.com>
2025-03-23 20:51:51 -0400

    Add files via upload

---
+++ Migrations/20250324001444_NewPropertie.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewPropertie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comentario",
+                table: "Tareas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comentario",
+                table: "Tareas");
+        }
+    }
+}
